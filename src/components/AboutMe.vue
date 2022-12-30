fbd1b2b019706aef2d90c6d6782d2063c5ef6882
Olamide Olajide <82158924+olamideolajide01@users.noreply.github.com>
2022-12-30 19:42:40 +0100

    Update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -8,16 +8,18 @@
     </v-row  >
  <v-row justify="space-around"  class="mx-auto">
          <v-col cols="12" sm="12" md="6" lg="6" class="pl-16" >
-   <p class="white--text mr-12"> Hello! My name is Olamide, i'm a Nigerian-Based software developer and I enjoy creating things that live on the internet. i started  teaching myself alot about HTML & CSS in 2011 when i took interest in Coding..</p>
+   <p class="white--text mr-12"> Hello! My name is Olamide, I'm a Nigerian-Based software developer and I enjoy creating things that live on the internet...</p>
    <p class="white--text">
-   Fast-forward to today,  i've had the opportunities of creating website apps with different technologies.. My main focus these days is building accessible and a user experience products for a variety of clients.</p>
+   Fast-forward to today,  I've had the opportunities of creating website apps with different technologies.. My main focus these days is building accessible and a user experience products for a variety of clients.</p>
   <p class="white--text"> Here are a few technologies I've been working with recently:</p>
   
   <ul class="white--text mx-auto">
+  <li>HTML</li> 
+  <li>CSS</li> 
     <li>JavaScript (ES6+)</li> 
    <li>Vue</li> 
-   <li>Node.js</li> 
-    <li> WordPress</li> 
+   <li>React.js</li> 
+   <li>Next.js</li> 
 </ul>
      </v-col>
 
